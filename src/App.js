5588595c491ff05b5420421e8d3fcdc305551518
Srinithi Sasikumar <sasikumarsrinithi09@gmail.com>
2026-02-13 12:51:43 +0530

    button component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from './components/Button/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="Contact Us" bgColor="red" />
+      <Button label="Read More" bgColor="orange" />
+    </div>
+  );
+}
+
+export default App;
